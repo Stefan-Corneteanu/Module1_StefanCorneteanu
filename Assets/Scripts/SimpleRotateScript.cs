9064c4df268e1d9ddaae72d3221bb56410ec3df0
Stefan-Corneteanu <93118793+Stefan-Corneteanu@users.noreply.github.com>
2023-12-28 17:10:30 +0200

    Initial commit

---
+++ Assets/Scripts/SimpleRotateScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotateScript : MonoBehaviour
+{
+
+    public float rot = 0.25f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(Vector3.up*rot); 
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Module1_StefanCorneteanu
+ My implementation for XAMK's IVGC Module 1 assignment
